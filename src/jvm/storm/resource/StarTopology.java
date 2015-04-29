4c5a40afcbba34c2bb5ce47088824ddcca4f0014
Jerry <jerry@ubuntu.(none)>
2015-04-29 12:05:53 -0700

    testing

--- src/jvm/storm/resource/StarTopology.java
+++ src/jvm/storm/resource/StarTopology.java
@@ -12,7 +12,7 @@ public class StarTopology {
 	public static void main(String[] args) throws Exception {
 		int numSpout = 2;
 		int numBolt = 2;
-		int paralellism = 3;
+		int paralellism = 3*2;
 
 		TopologyBuilder builder = new TopologyBuilder();
 
