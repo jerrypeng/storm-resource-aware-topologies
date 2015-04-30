51592a60503e51305a99dbf55a6e04c0b781c7d3
Jerry <jerry@ubuntu.(none)>
2015-04-29 20:28:46 -0700

    testing

--- src/jvm/storm/resource/DiamondTopology.java
+++ src/jvm/storm/resource/DiamondTopology.java
@@ -15,7 +15,7 @@ public class DiamondTopology {
         TopologyBuilder builder = new TopologyBuilder();
 
         SpoutDeclarer spout = builder.setSpout("spout_head", new TestSpout(), paralellism);
-        spout.setCPULoad(50.0);
+        spout.setCPULoad(40.0);
 
        BoltDeclarer bolt1 = builder.setBolt("bolt_1", new TestBolt(), paralellism);
        BoltDeclarer bolt2 = builder.setBolt("bolt_2", new TestBolt(), paralellism);
@@ -23,16 +23,16 @@ public class DiamondTopology {
        BoltDeclarer bolt4 = builder.setBolt("bolt_4", new TestBolt(), paralellism);
        
        bolt1.shuffleGrouping("spout_head");
-       bolt1.setCPULoad(15.0);
+       bolt1.setCPULoad(20.0);
        
        bolt2.shuffleGrouping("spout_head");
-       bolt2.setCPULoad(15.0);
+       bolt2.setCPULoad(20.0);
        
        bolt3.shuffleGrouping("spout_head");
-       bolt3.setCPULoad(15.0);
+       bolt3.setCPULoad(20.0);
        
        bolt4.shuffleGrouping("spout_head");
-       bolt4.setCPULoad(15.0);
+       bolt4.setCPULoad(20.0);
 
         BoltDeclarer output = builder.setBolt("bolt_output_3", new TestBolt(), paralellism);
         output.shuffleGrouping("bolt_1");
