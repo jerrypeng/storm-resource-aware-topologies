8d4080347f38f0af3e9024d6a2540db42c2bcbfb
Jerry <jerry@ubuntu.(none)>
2015-04-30 15:26:57 -0700

    testing

--- src/jvm/storm/resource/LinearTopology.java
+++ src/jvm/storm/resource/LinearTopology.java
@@ -142,7 +142,7 @@ public class LinearTopology {
     BoltDeclarer bolt_2 = builder.setBolt("exclaim2", new LinearBolt(1,2,5), 6);
     BoltDeclarer bolt_3 = builder.setBolt("exclaim_output_3", new LinearBolt(1,2,5), 6);
     
-    spout.setCPULoad(40.0);
+    spout.setCPULoad(50.0);
     
     bolt_1.shuffleGrouping("word");
     bolt_1.setCPULoad(20.0);
